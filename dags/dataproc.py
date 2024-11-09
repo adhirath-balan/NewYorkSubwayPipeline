d7d128a71d145641a948d580f75502154e1e7a99
Adhirath <adhirath.balan@gmail.com>
2024-11-09 19:20:55 +0100

    Running pipeline

--- dags/dataproc.py
+++ dags/dataproc.py
@@ -1,8 +1,8 @@
 from google.cloud import dataproc_v1 as dataproc
 
 # Configuration parameters
-PROJECT_ID = "data-management-3-440208"  # Replace with your Google Cloud project ID omega-keep-411319
-CLUSTER_NAME = "data-management-3"
+PROJECT_ID = "adb-dm2"  # Replace with your Google Cloud project ID omega-keep-411319
+CLUSTER_NAME = "cluster-51df"
 REGION = "us-central1"  # Replace with your cluster's region
 
 def run_dataproc_job(CLUSTER_NAME, PROJECT_ID, REGION):
@@ -15,7 +15,7 @@ def run_dataproc_job(CLUSTER_NAME, PROJECT_ID, REGION):
 
     # Initialize request argument(s)
     job = dataproc.Job()
-    job.pyspark_job.main_python_file_uri = "gs://data_management_3/preprocessing_spark.py"
+    job.pyspark_job.main_python_file_uri = "gs://data_management_2/preprocessing_spark.py"
     job.placement.cluster_name = CLUSTER_NAME
 
     request = dataproc.SubmitJobRequest(
--- dags/preprocessing_spark.py
+++ dags/preprocessing_spark.py
@@ -2,14 +2,14 @@ from pyspark.sql import SparkSession, functions as f
 
 spark = SparkSession.builder\
         .master("yarn")\
-        .appName('DataManagement3')\
+        .appName('DataManagement2')\
         .getOrCreate()
 
 df = spark.read.csv(
-    "gs://data_management_3/*.csv",
+    "gs://data_management_2/*.csv",
     sep = ",",
     header = True
 )
 
 df = df.drop("departure_time")
-df.write.csv("gs://data_management_3/final_data.csv")
+df.write.csv("gs://data_management_2/final_data.csv")
